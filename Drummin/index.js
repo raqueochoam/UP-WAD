088801def5999b8bbd184288194d6136f4119b60
raqueochoam <raqueochoam@gmail.com>
2021-09-22 17:37:34 -0500

    22/09/2021

---
+++ Drummin/index.js
@@ -0,0 +1,63 @@
+function makeSound(key){
+    var soundfile;
+
+    switch(soundfile){
+        case 'w' :
+            soundfile="tom-1";
+            break;
+
+        case 'a' :
+            soundfile="tom-2";
+            break;
+
+        case 's' :
+            soundfile="tom-3";
+            break;
+
+        case 'd' :
+            soundfile="tom-4";
+            break;
+
+        case 'j' :
+            soundfile="snare";
+            break;
+
+        case 'k' :
+            soundfile="crash";
+            break;
+
+        case 'l' :
+            soundfile="kick-bass";
+            break;
+    }
+
+    var sound = new Audio("sounds/" + soundfile + ".mp3");
+    sound.play();
+
+}
+
+function animationBtn(currentKey){
+
+    var activeBtn = document.querySelector("." + currentKey);
+
+    activeBtn.classList.add("pressed");
+    setTimeout(function(){
+        activeBtn.classList.remove("pressed");
+    },100);
+}
+
+var drums = document.querySelectorAll(".drum");
+var numberOfDrums = drums.length();
+
+for (var i=0; i<numberOfDrums; i++){
+    drums[i].addEventListener("click", function(){
+        var keyPressed = this.innerHTML;
+        makeSound(keyPressed);
+        animationBtn(keyPressed);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    animationBtn(event.key);
+});
